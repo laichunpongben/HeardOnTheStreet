56ca5d4a75de2d8d8db88e5afddbc4313a0d0803
laichunpongben <laichunpongben@gmail.com>
2015-10-29 19:15:33 +0800

    Update and rename 4_26.py to q4_26.py

--- q4_26.py
+++ q4_26.py
@@ -1,3 +1,6 @@
+# Heard on the Street
+# Q4.26
+
 # You will roll a fair diece until the game stops.
 # The game stops when you get a 4, 5, or 6.
 # For every number 1, 2, or 3 you have thrown your score increases by +1. 
@@ -8,33 +11,33 @@
 import random
 
 payoff = 0
-isNext = True
+is_next = True
 
-def UpdatePayoff(x):
+def update_payoff(x):
     global payoff 
-    global isNext
+    global is_next
     if x <= 3:
         payoff += 1
     elif x == 6:
         payoff = 0
-        isNext = False
+        is_next = False
     else:
-        isNext = False
+        is_next = False
 
-def PlayGame():
-    while isNext:
-        sampleDice = random.randint(1, 6)
-        UpdatePayoff(sampleDice)
+def play_game():
+    while is_next:
+        sample_dice = random.randint(1, 6)
+        update_payoff(sample_dice)
 
-def PlayManyGames(gameCount):
+def play_many_games(game_count):
     global payoff
-    global isNext
-    payoffTotal = 0
-    for x in range(gameCount):
-        PlayGame()
-        payoffTotal += payoff
+    global is_next
+    payoff_total = 0
+    for x in range(game_count):
+        play_game()
+        payoff_total += payoff
         payoff = 0
-        isNext = True
-    print(payoffTotal / gameCount)
+        is_next = True
+    print(payoff_total / game_count)
 
-PlayManyGames(100000)
+play_many_games(100000)
